a2b3ff7b8e309e27cd3b76747fd4177371c7ce7d
Jory Graham <jory.graham@gmail.com>
2015-02-24 16:44:51 -0500

    Eventable?

--- ttyplayer.js
+++ ttyplayer.js
@@ -3,15 +3,21 @@ var util = require('util');
 var ansi = require('ansi'),
     cursor = ansi(process.stdout);
 
+var keypress = require('keypress');
+keypress(process.stdin);
+
 var fileParser = require('./file-parser');
 var ttyrecDecoder = require('./ttyrec-decoder');
 
 module.exports = function (file) {
     fileParser(file, function (err, parsed) {
         ttyrecDecoder(parsed, function (err, ttyrec) {
-            var index = 0;
-            var blugh = function () {
+
+            var printFrame = function (index) {
                 console.log('\u001B[2J\u001B[0;0f');
+
+                console.log("~> ", index);
+
                 var frame = ttyrec.frames[index];
                 for (var i = 0, il = frame.length; i < il; i++) {
                     var row = frame[i];
@@ -23,16 +29,26 @@ module.exports = function (file) {
                         cursor[char.foreground]();
                         cursor.bg[char.background]();
                         cursor.write(char.char);
+                        cursor.reset();
                     }
                     cursor.write("\n");
                 }
-
-                if (index < ttyrec.frames.length) {
-                    index++;
-                    setTimeout(blugh, 0);
-                }
             };
-            blugh();
+
+
+            var index = 0;
+
+            process.stdin.on('keypress', function (ch, key) {
+                console.log('got "keypress"', key);
+                if (key && key.ctrl && key.name == 'c') {
+                    process.stdin.pause();
+                } else {
+                    printFrame(index++);
+                }
+            });
+
+            process.stdin.setRawMode(true);
+            process.stdin.resume();
         });
     });
 };
