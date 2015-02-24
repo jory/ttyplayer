5faa1bb694104182ea723151d109164d39128cdb
Jory Graham <jory.graham@gmail.com>
2015-02-23 23:40:36 -0500

    Yus!

--- ttyplayer.js
+++ ttyplayer.js
@@ -1,3 +1,5 @@
+var util = require('util');
+
 var fileParser = require('./file-parser');
 var ttyrecDecoder = require('./ttyrec-decoder');
 
@@ -7,9 +9,12 @@ module.exports = function (file) {
             console.log(Object.keys(ttyrec));
             console.log(ttyrec.frames.length);
 
-            for (var p = 0, pl = ttyrec.frames.length; p < pl; p++) {
-                var frame = ttyrec.frames[p];
+            var index = 0;
 
+            var blugh = function () {
+                console.log('\u001B[2J\u001B[0;0f');
+
+                var frame = ttyrec.frames[index];
                 var string = "";
 
                 for (var i = 0, il = frame.length; i < il; i++) {
@@ -28,7 +33,14 @@ module.exports = function (file) {
                 }
 
                 console.log(string);
-            }
+
+                if (index < ttyrec.frames.length) {
+                    index++;
+                    setTimeout(blugh, 0);
+                }
+            };
+
+            blugh();
         });
     });
 };
